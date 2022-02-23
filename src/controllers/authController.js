e78b172128aac56ffd908bcdd55ab2c48342f9f4
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-02-23 01:47:19 -0300

    applying prettier and filtering cleaning console.logs

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const jwt = require('jsonwebtoken')
-const authConfig = require("../config/auth.json")
-const authSecretary = require("../middlewares/authSecretary")
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.json");
+const authSecretary = require("../middlewares/authSecretary");
 
-const { PrismaClient } = require("@prisma/client")
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
@@ -11,117 +11,143 @@ const authenticationRoute = express.Router();
 
 // Register Secretary
 authenticationRoute.post("/secretary", async (request, response) => {
-    const {email, password} = request.body
+  const { email, password } = request.body;
 
-    const secretaryAlreadyExist = await prisma.secretary.findUnique({ where: { email }});
-    const doctorAlreadyExist = await prisma.doctor.findUnique({ where: { email }});
+  const secretaryAlreadyExist = await prisma.secretary.findUnique({
+    where: { email },
+  });
+  const doctorAlreadyExist = await prisma.doctor.findUnique({
+    where: { email },
+  });
 
-    if(secretaryAlreadyExist || doctorAlreadyExist){
-        return response.status(404).json("Email already used")
-    }
+  if (secretaryAlreadyExist || doctorAlreadyExist) {
+    return response.status(404).json("Email already used");
+  }
 
-    const secretary = await prisma.secretary.create({
-        data: {
-            email, password
-        },
-    });
+  const secretary = await prisma.secretary.create({
+    data: {
+      email,
+      password,
+    },
+  });
 
-    secretary.password = undefined;
+  secretary.password = undefined;
 
-    return response.status(201).send({
-        secretary, 
-    })
+  return response.status(201).send({
+    secretary,
+  });
 });
 
 // Register Doctor
-authenticationRoute.post("/doctors", authSecretary, async (request, response) => {
-    console.log(request.body)
-    const {name, email} = request.body
+authenticationRoute.post(
+  "/doctors",
+  authSecretary,
+  async (request, response) => {
+    const { name, email } = request.body;
 
-    const secretaryAlreadyExist = await prisma.secretary.findUnique({ where: { email }});
-    const doctorAlreadyExist = await prisma.doctor.findUnique({ where: { email }});
+    const secretaryAlreadyExist = await prisma.secretary.findUnique({
+      where: { email },
+    });
+    const doctorAlreadyExist = await prisma.doctor.findUnique({
+      where: { email },
+    });
 
-    if(doctorAlreadyExist || secretaryAlreadyExist){
-        return response.status(404).json("Email already used")
+    if (doctorAlreadyExist || secretaryAlreadyExist) {
+      return response.status(404).json("Email already used");
     }
 
     const doctor = await prisma.doctor.create({
-        data: {
-            name,
-            email
-        },
+      data: {
+        name,
+        email,
+      },
     });
 
     return response.status(201).send({
-        doctor, 
-    })
-});
+      doctor,
+    });
+  }
+);
 
 // Secretary Login
-authenticationRoute.post('/authenticate/secretary', async(request, response) => {
-    const {email, password} = request.body;
+authenticationRoute.post(
+  "/authenticate/secretary",
+  async (request, response) => {
+    const { email, password } = request.body;
 
-    if(!email)
-        return response.status(400).send("Email field not filled")
-    
-    if(!password)
-        return response.status(400).send("Password field not filled")
+    if (!email) return response.status(400).send("Email field not filled");
 
-    const secretaryAlreadyExist = await prisma.secretary.findUnique({ where: { email }});;
+    if (!password)
+      return response.status(400).send("Password field not filled");
 
-    if(!secretaryAlreadyExist){
-        return response.status(404).json("Secretary not found")
+    var secretaryAlreadyExist = await prisma.secretary.findUnique({
+      where: { email },
+    });
+
+    if (!secretaryAlreadyExist) {
+      return response.status(404).json("Secretary not found");
     }
 
-    if(secretaryAlreadyExist.password != password){
-        return response.status(400).send({ error: 'Invalid password'});
+    if (secretaryAlreadyExist.password != password) {
+      return response.status(400).send({ error: "Invalid password" });
     }
-    console.log(secretaryAlreadyExist)
 
     const secret = authConfig.secret;
 
-    const token = jwt.sign({
+    const token = jwt.sign(
+      {
         userType: "secretary",
         id: secretaryAlreadyExist.id,
-    },
-    secret, {
+      },
+      secret,
+      {
         expiresIn: 86400,
-    })
+      }
+    );
+
+    secretaryAlreadyExist = {
+      id: secretaryAlreadyExist.id,
+      email: secretaryAlreadyExist.email,
+    };
 
     return response.status(200).send({
-        secretaryAlreadyExist, 
-        token
-    })
-    
-})
+      secretaryAlreadyExist,
+      token,
+    });
+  }
+);
 
 // Doctor Login
-authenticationRoute.post('/authenticate/doctor', async(request, response) => {
-    const {email} = request.body
+authenticationRoute.post("/authenticate/doctor", async (request, response) => {
+  const { email } = request.body;
 
-    if(!email)
-        return response.status(400).send("Email field not filled")
+  if (!email) return response.status(400).send("Email field not filled");
 
-    const doctorAlreadyExist = await prisma.doctor.findUnique({ where: { email }});
+  const doctorAlreadyExist = await prisma.doctor.findUnique({
+    where: { email },
+  });
 
-    if(!doctorAlreadyExist){
-        return response.status(404).json("Doctor not found")
-    }
+  if (!doctorAlreadyExist) {
+    return response.status(404).json("Doctor not found");
+  }
 
-    const secret = authConfig.secret;
+  const secret = authConfig.secret;
 
-    const token = jwt.sign({
-        userType: "doctor",
-        id: doctorAlreadyExist.id,
+  const token = jwt.sign(
+    {
+      userType: "doctor",
+      id: doctorAlreadyExist.id,
     },
-    secret, {
-        expiresIn: 86400,
-    })
+    secret,
+    {
+      expiresIn: 86400,
+    }
+  );
 
-    return response.status(200).send({
-        doctorAlreadyExist, 
-        token
-    })
+  return response.status(200).send({
+    doctorAlreadyExist,
+    token,
+  });
 });
 
 module.exports = authenticationRoute;
--- src/secretarys.routes.js
+++ src/secretarys.routes.js
@@ -1,179 +1,218 @@
-const express = require("express")
-const authSecretary = require("./middlewares/authSecretary")
+const express = require("express");
+const authSecretary = require("./middlewares/authSecretary");
 
-const secretaryRoutes = express.Router()
-const { PrismaClient } = require("@prisma/client")
+const secretaryRoutes = express.Router();
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 // Read Secretarys
 secretaryRoutes.get("/secretarys", async (request, response) => {
-    const secretary = await prisma.secretary.findMany({select: {
-        email: true,
-        password: false,
-    }})
-    return response.status(200).json(secretary);
-})
+  const secretary = await prisma.secretary.findMany({
+    select: {
+      email: true,
+      password: false,
+    },
+  });
+  return response.status(200).json(secretary);
+});
 
 // Update Secretary
 secretaryRoutes.put("/secretary", async (request, response) => {
-    const {email, password, id} = request.body
+  const { email, password, id } = request.body;
 
-    if(!id){
-        return response.status(400).json("Id is mandatory")
-    }
+  if (!id) {
+    return response.status(400).json("Id is mandatory");
+  }
 
-    const secretaryAlreadyExist = await prisma.secretary.findUnique({ where: { id }});
+  const secretaryAlreadyExist = await prisma.secretary.findUnique({
+    where: { id },
+  });
 
-    if(!secretaryAlreadyExist) {
-        return response.status(404).json("Secretary not exists")
-    }
+  if (!secretaryAlreadyExist) {
+    return response.status(404).json("Secretary not exists");
+  }
 
-    const secretary = await prisma.secretary.update({
-        where: {
-            id,
-        },
-        data: {
-            email,
-            password
-        }
-    })
+  const secretary = await prisma.secretary.update({
+    where: {
+      id,
+    },
+    data: {
+      email,
+      password,
+    },
+  });
 
-    return response.status(200).json(secretary)
-})
+  return response.status(200).json(secretary);
+});
 
 // Delete Secretary
-secretaryRoutes.delete("/secretary/:id",  async  (request, response) => {
-    const {id} = request.params;
-    const intId = parseInt(id);
+secretaryRoutes.delete("/secretary/:id", async (request, response) => {
+  const { id } = request.params;
+  const intId = parseInt(id);
 
-    if(!intId){
-        return response.status(400).json("Id is mandatory")
-    }
+  if (!intId) {
+    return response.status(400).json("Id is mandatory");
+  }
 
-    const secretaryAlreadyExist = await prisma.secretary.findUnique({ where: { id: intId }});
+  const secretaryAlreadyExist = await prisma.secretary.findUnique({
+    where: { id: intId },
+  });
 
-    if(!secretaryAlreadyExist) {
-        return response.status(404).json("Secretary not exists")
-    }
+  if (!secretaryAlreadyExist) {
+    return response.status(404).json("Secretary not exists");
+  }
 
-    await prisma.secretary.delete({ where: { id: intId }});
-    return response.status(200).send();
+  await prisma.secretary.delete({ where: { id: intId } });
+  return response.status(200).send();
 });
 
 // Read doctors
-secretaryRoutes.get("/secretary/doctors", authSecretary, async (request, response) => {
-    console.log(request.userType)
-    if(request.userType != 'secretary')
-        return response.status(400).json("Permission denied")
-    const doctors = await prisma.doctor.findMany()
-    return response.status(200).json({doctors: doctors, user: request.userId});
-})
+secretaryRoutes.get(
+  "/secretary/doctors",
+  authSecretary,
+  async (request, response) => {
+    if (request.userType != "secretary")
+      return response.status(400).json("Permission denied");
+    const doctors = await prisma.doctor.findMany();
+    return response
+      .status(200)
+      .json({ doctors: doctors, user: request.userId });
+  }
+);
 
 // Update doctor
-secretaryRoutes.put("/secretary/doctors", authSecretary, async (request, response) => {
-    if(request.userType != 'secretary')
-        return response.status(400).json("Permission denied")
-    const {name, id} = request.body
+secretaryRoutes.put(
+  "/secretary/doctors",
+  authSecretary,
+  async (request, response) => {
+    if (request.userType != "secretary")
+      return response.status(400).json("Permission denied");
+    const { name, id } = request.body;
 
-    if(!id){
-        return response.status(400).json("Id is mandatory")
+    if (!id) {
+      return response.status(400).json("Id is mandatory");
     }
 
-    const doctorAlreadyExist = await prisma.doctor.findUnique({ where: { id }});
+    const doctorAlreadyExist = await prisma.doctor.findUnique({
+      where: { id },
+    });
 
-    if(!doctorAlreadyExist) {
-        return response.status(404).json("Doctor not exists")
+    if (!doctorAlreadyExist) {
+      return response.status(404).json("Doctor not exists");
     }
 
     const doctor = await prisma.doctor.update({
-        where: {
-            id,
-        },
-        data: {
-            name,
-        }
-    })
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    });
 
-    return response.status(200).json(doctor)
-})
+    return response.status(200).json(doctor);
+  }
+);
 
 // Delete Doctor
-secretaryRoutes.delete("/secretary/doctor/:id", authSecretary, async (request, response) => {
-    if(request.userType != 'secretary')
-        return response.status(400).json("Permission denied")
-    const {id} = request.params;
+secretaryRoutes.delete(
+  "/secretary/doctor/:id",
+  authSecretary,
+  async (request, response) => {
+    if (request.userType != "secretary")
+      return response.status(400).json("Permission denied");
+    const { id } = request.params;
     const intId = parseInt(id);
 
-    if(!intId){
-        return response.status(400).json("Id is mandatory")
+    if (!intId) {
+      return response.status(400).json("Id is mandatory");
     }
 
-    const doctorAlreadyExist = await prisma.doctor.findUnique({ where: { id: intId }});
+    const doctorAlreadyExist = await prisma.doctor.findUnique({
+      where: { id: intId },
+    });
 
-    if(!doctorAlreadyExist) {
-        return response.status(404).json("Doctor not exists")
+    if (!doctorAlreadyExist) {
+      return response.status(404).json("Doctor not exists");
     }
 
-    await prisma.doctor.delete({ where: { id: intId }});
+    await prisma.doctor.delete({ where: { id: intId } });
     return response.status(200).send();
-});
+  }
+);
 
 // Create appointment
-secretaryRoutes.post("/secretary/appointment", authSecretary, async (request, response) => {
-    if(request.userType != 'secretary')
-        return response.status(400).json("Permission denied")
-    const {doctorId, patientName, date} = request.body;
+secretaryRoutes.post(
+  "/secretary/appointment",
+  authSecretary,
+  async (request, response) => {
+    if (request.userType != "secretary")
+      return response.status(400).json("Permission denied");
+    const { doctorId, patientName, date } = request.body;
 
     const appointment = await prisma.appointment.create({
-        data: {
-            doctorId, patientName, date
-        },
+      data: {
+        doctorId,
+        patientName,
+        date,
+      },
     });
-    return response.status(201).json({appointment: appointment, user: request.userId})
-});
+    return response
+      .status(201)
+      .json({ appointment: appointment, user: request.userId });
+  }
+);
 
 // Read appointments
-secretaryRoutes.get("/secretary/appointments", authSecretary, async (request, response) => {
-    if(request.userType != 'secretary')
-        return response.status(400).json("Permission denied")
-    const appointment = await prisma.appointment.findMany(
-        {
-            orderBy: {
-                date: 'asc'
-            },
-        }
-    )
+secretaryRoutes.get(
+  "/secretary/appointments",
+  authSecretary,
+  async (request, response) => {
+    if (request.userType != "secretary")
+      return response.status(400).json("Permission denied");
+    const appointment = await prisma.appointment.findMany({
+      orderBy: {
+        date: "asc",
+      },
+    });
     return response.status(200).json(appointment);
-})
+  }
+);
 
 // Update appointment
-secretaryRoutes.put("/secretary/appointment", authSecretary, async (request, response) => {
-    if(request.userType != 'secretary')
-        return response.status(400).json("Permission denied")
-    const {date, patientName, id} = request.body
+secretaryRoutes.put(
+  "/secretary/appointment",
+  authSecretary,
+  async (request, response) => {
+    if (request.userType != "secretary")
+      return response.status(400).json("Permission denied");
+    const { date, patientName, id } = request.body;
 
-    if(!id){
-        return response.status(400).json("Id is mandatory")
+    if (!id) {
+      return response.status(400).json("Id is mandatory");
     }
 
-    const appointmentAlreadyExist = await prisma.appointment.findUnique({ where: { id }});
+    const appointmentAlreadyExist = await prisma.appointment.findUnique({
+      where: { id },
+    });
 
-    if(!appointmentAlreadyExist) {
-        return response.status(404).json("Appointment not exists")
+    if (!appointmentAlreadyExist) {
+      return response.status(404).json("Appointment not exists");
     }
 
     const appointment = await prisma.appointment.update({
-        where: {
-            id,
-        },
-        data: {
-            date,
-            patientName
-        }
-    })
+      where: {
+        id,
+      },
+      data: {
+        date,
+        patientName,
+      },
+    });
 
-    return response.status(200).json(appointment)
-})
+    return response.status(200).json(appointment);
+  }
+);
 
 module.exports = secretaryRoutes;
