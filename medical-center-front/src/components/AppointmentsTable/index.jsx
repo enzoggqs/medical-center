4a7b921c7d65209aea93915553d47ef0d4fc6bac
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-02-26 00:41:36 -0300

    Creating appointments table component

---
+++ medical-center-front/src/components/AppointmentsTable/index.jsx
@@ -0,0 +1,39 @@
+const AppointmentsTable = ({doctors, appointments}) => {
+    return (
+        <div className="bg-white text-red-400 w-2/3 ml-auto mr-auto">
+            {
+                doctors.length == 0 || appointments.length == 0 ?
+                    (<h1>Carregando dados...</h1>)
+                :
+                <table className="w-full border-2 border-red-400">
+                    <thead>
+                        <tr>
+                            <th className="border-2 border-red-400">Nome do Paciente</th>
+                            <th className="border-2 border-red-400">Nome do Doutor</th>
+                            <th className="border-2 border-red-400">Data da Consulta</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {appointments.map((appointment) => (
+                            <tr key={appointment.id} className="border-2 border-red-400">
+                                <td className="border-2 border-red-400">
+                                    {appointment.patientName}
+                                </td>
+                                <td className="border-2 border-red-400">
+                                    {doctors.find((doctor) => doctor.id === appointment.doctorId).name}
+                                </td>
+                                <td className="border-2 border-red-400">
+                                    {appointment.date.slice(0, 10).split("-").reverse().join("-").replace(/-/g, "/")}
+                                </td>
+                            </tr>
+                        )
+
+                        )}
+                    </tbody>
+                </table>
+            }
+        </div>
+    )
+}
+
+export default AppointmentsTable;
