1a30b10e9611c5cfe1b08e16380d5a67fb4b3f86
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-02-28 10:46:50 -0300

    Adding doctorPage to HomePage

--- medical-center-front/src/pages/HomePage/index.jsx
+++ medical-center-front/src/pages/HomePage/index.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import AppointmentsTable from "../../components/AppointmentsTable";
 import DoctorsList from "../../components/DoctorsList";
 import FormAddDoctor from "../../components/FormAddDoctor";
+import FormDoctorCreateAppointment from "../../components/FormDoctorCreateAppointment";
+import FormDoctorEditAppointment from "../../components/FormDoctorEditAppointment";
 import FormEditDoctor from "../../components/FormEditDoctor";
 import FormSecretaryCreateAppointment from "../../components/FormSecretaryCreateAppointment";
 import FormSecretaryEditAppointment from "../../components/FormSecretaryEditAppointment";
@@ -18,11 +20,15 @@ import {
   editDoctor,
   deleteDoctor,
   secretaryEditAppointment,
-  secretaryDeleteAppointment
+  secretaryDeleteAppointment,
+  getDoctorAppointments,
+  doctorCreateAppointment,
+  doctorEditAppointment,
+  doctorDeleteAppointment
 } from "../../services/api";
 
 const HomePage = () => {
-    const { authenticated, logout, user } = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
     const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -38,7 +44,6 @@ const HomePage = () => {
     const [showDoctorToChangeFields, setShowDoctorToChangeFields] = useState(false);
     const [doctorNewName, setDoctorNewName] = useState(null);
 
-    const [showAppointmentToChangeFields, setShowAppointmentToChangeFields] = useState(false);
     const [appointmentToChange, setAppointmentToChange] = useState(null);
     const [appointmentToChangeDate, setAppointmentToChangeDate] = useState(null);
     const [appointmentsForDate, setAppointmentsForDate] = useState(null);
@@ -46,18 +51,25 @@ const HomePage = () => {
     const [appointmentNewDate, setAppointmentNewDate] = useState('');
 
     useEffect(() => {
-        console.log(user, appointments);
+        console.log(user);
         if(user.userType === 2){
             (async () => {
                 const allDoctors = await getDoctors();
                 setDoctors(allDoctors.data.doctors);
 
                 const allAppointments =  await getSecretaryAppointments();
-                console.log(allAppointments.data, allDoctors.data)
                 setAppointments(allAppointments.data);
                 
                 setLoading(false);
             })();
+        }
+        if(user.userType == 1){
+          (async () => {
+            const allAppointments =  await getDoctorAppointments();
+            setAppointments(allAppointments.data);
+            setAppointmentDoctor(user.id)
+            setLoading(false);
+        })();
         }
     }, [])
 
@@ -80,11 +92,9 @@ const HomePage = () => {
     
     useEffect(() => {
     if(appointmentToChange !== null) {
-      setShowAppointmentToChangeFields(true);
       setAppointmentNewPatient(appointments.find((ap) => ap.id == appointmentToChange).patientName)
 
     }else {
-      setShowAppointmentToChangeFields(false);
       setAppointmentNewPatient('Digite o novo nome do paciente')
       setAppointmentNewDate('')
     }
@@ -92,7 +102,6 @@ const HomePage = () => {
     }, [appointmentToChange])
 
     const handleCreateDoctor = async (e) => {
-        console.log('oi')
         e.preventDefault();
 
         await createDoctor(doctorToCreateName, doctorToCreateEmail)
@@ -106,10 +115,14 @@ const HomePage = () => {
       e.preventDefault();
       var date = new Date(appointmentDate); 
 
-      // console.log(appointmentPatient, appointmentDoctor, date.toISOString())
 
-      await secretaryCreateAppointment(appointmentPatient, appointmentDoctor, date.toISOString())
-      const allAppointments =  await getSecretaryAppointments();
+      if(user.userType == 2){
+        await secretaryCreateAppointment(appointmentPatient, appointmentDoctor, date.toISOString())
+        var allAppointments =  await getSecretaryAppointments();
+      } else {
+        await doctorCreateAppointment(appointmentDoctor, appointmentPatient, date.toISOString())
+        var allAppointments =  await getDoctorAppointments();
+      }
       await setAppointments(allAppointments.data);
       await setAppointmentPatient('');
       await setAppointmentDoctor(null);
@@ -129,7 +142,6 @@ const HomePage = () => {
     const handleDeleteDoctor = async (e) => {
       e.preventDefault();
 
-      console.log(doctorToChange);
       await deleteDoctor(doctorToChange);
 
       const allDoctors = await getDoctors();
@@ -146,24 +158,41 @@ const HomePage = () => {
       e.preventDefault();
       var date = new Date(appointmentNewDate);
 
-      if(!appointmentNewDate) {
-        await secretaryEditAppointment(
-          0, 
-          appointmentNewPatient, 
-          parseInt(appointmentToChange)
-        )
+      if(user.userType == 2){
+        if(!appointmentNewDate) {
+          await secretaryEditAppointment(
+            0, 
+            appointmentNewPatient, 
+            parseInt(appointmentToChange)
+          )
+        } else {
+          await secretaryEditAppointment(
+            date, 
+            appointmentNewPatient, 
+            parseInt(appointmentToChange)
+          )
+        }
+        var allAppointments =  await getSecretaryAppointments();
+        const allDoctors = await getDoctors();
+        await setDoctors(allDoctors.data.doctors);
       } else {
-        await secretaryEditAppointment(
-          date, 
-          appointmentNewPatient, 
-          parseInt(appointmentToChange)
-        )
+        if(!appointmentNewDate) {
+          await doctorEditAppointment(
+            0, 
+            appointmentNewPatient, 
+            parseInt(appointmentToChange)
+          )
+        } else {
+          await doctorEditAppointment(
+            date, 
+            appointmentNewPatient, 
+            parseInt(appointmentToChange)
+          )
+        }
+        var allAppointments =  await getDoctorAppointments();
       }
 
-      const allAppointments =  await getSecretaryAppointments();
       await setAppointments(allAppointments.data);
-      const allDoctors = await getDoctors();
-      await setDoctors(allDoctors.data.doctors);
       await setAppointmentToChangeDate(null)
       await setAppointmentsForDate(null)
       await setAppointmentToChange(null)
@@ -172,14 +201,13 @@ const HomePage = () => {
     const handleSecretaryDeleteAppointment = async (e) => {
       e.preventDefault();
 
-      console.log(appointmentToChange)
       await secretaryDeleteAppointment(appointmentToChange);
 
       const allAppointments =  await getSecretaryAppointments();
       setAppointments(allAppointments.data);
 
       await setAppointmentToChange(null)
-      await setShowAppointmentToChangeFields(false);
+      await setAppointmentToChangeDate(null)
     }
 
     if(loading) {
@@ -190,60 +218,90 @@ const HomePage = () => {
         <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
           <div className="flex w-full flex-1 flex-col items-center justify-center text-center">
             <Header />
-            <div className="bg-white flex w-full">
-              <FormAddDoctor 
-                setDoctorToCreateName={setDoctorToCreateName} 
-                setDoctorToCreateEmail={setDoctorToCreateEmail} 
-                doctorToCreateName={doctorToCreateName} 
-                doctorToCreateEmail={doctorToCreateEmail}
-                handleCreateDoctor={handleCreateDoctor}
-              />
-              <FormSecretaryCreateAppointment
-                setAppointmentDoctor={setAppointmentDoctor}
-                setAppointmentPatient={setAppointmentPatient}
-                setAppointmentDate={setAppointmentDate}
-                doctors={doctors}
-                appointmentDoctor={appointmentDoctor}
-                appointmentPatient={appointmentPatient}
-                appointmentDate={appointmentDate}
-                handleCreateAppointment={handleCreateAppointment}
-              />     
-            </div>
-
-            <div className="flex w-full flex-1 flex-col items-center justify-center text-center">
-              <div className="bg-white flex w-full">
-                <FormEditDoctor 
-                  setDoctorToChange={setDoctorToChange}
-                  doctorToChange={doctorToChange}
-                  doctors={doctors}
-                  showDoctorToChangeFields={showDoctorToChangeFields}
-                  handleEditDoctor={handleEditDoctor}
-                  doctorNewName={doctorNewName}
-                  setDoctorNewName={setDoctorNewName}
-                  handleDeleteDoctor={handleDeleteDoctor}
-                />
-                <FormSecretaryEditAppointment 
-                  appointments={appointments}
-                  appointmentToChange={appointmentToChange}
-                  appointmentToChangeDate={appointmentToChangeDate}
-                  appointmentsForDate={appointmentsForDate}
-                  setAppointmentToChange={setAppointmentToChange}
-                  setAppointmentToChangeDate={setAppointmentToChangeDate}
-                  handleEditAppointment={handleEditAppointment}
-                  appointmentNewPatient={appointmentNewPatient}
-                  setAppointmentNewPatient={setAppointmentNewPatient}
-                  setAppointmentNewDate={setAppointmentNewDate}
-                  handleSecretaryDeleteAppointment={handleSecretaryDeleteAppointment}
-                />
-              </div>
-            </div>
-
-            <div className="flex w-full flex-1 flex-col items-center justify-center text-center">
-              <div className="bg-white flex w-full">
-                  <DoctorsList doctors={doctors}></DoctorsList>
-                  <AppointmentsTable doctors={doctors} appointments={appointments} />
-              </div>
-            </div>
+            {user.userType == 2 ? (
+              <>
+                <div className="bg-white flex w-full">
+                  <FormAddDoctor 
+                    setDoctorToCreateName={setDoctorToCreateName} 
+                    setDoctorToCreateEmail={setDoctorToCreateEmail} 
+                    doctorToCreateName={doctorToCreateName} 
+                    doctorToCreateEmail={doctorToCreateEmail}
+                    handleCreateDoctor={handleCreateDoctor}
+                  />
+                  <FormSecretaryCreateAppointment
+                    setAppointmentDoctor={setAppointmentDoctor}
+                    setAppointmentPatient={setAppointmentPatient}
+                    setAppointmentDate={setAppointmentDate}
+                    doctors={doctors}
+                    appointmentDoctor={appointmentDoctor}
+                    appointmentPatient={appointmentPatient}
+                    appointmentDate={appointmentDate}
+                    handleCreateAppointment={handleCreateAppointment}
+                  />     
+                </div>
+    
+                <div className="flex w-full flex-1 flex-col items-center justify-center text-center">
+                  <div className="bg-white flex w-full">
+                    <FormEditDoctor 
+                      setDoctorToChange={setDoctorToChange}
+                      doctorToChange={doctorToChange}
+                      doctors={doctors}
+                      showDoctorToChangeFields={showDoctorToChangeFields}
+                      handleEditDoctor={handleEditDoctor}
+                      doctorNewName={doctorNewName}
+                      setDoctorNewName={setDoctorNewName}
+                      handleDeleteDoctor={handleDeleteDoctor}
+                    />
+                    <FormSecretaryEditAppointment 
+                      appointments={appointments}
+                      appointmentToChange={appointmentToChange}
+                      appointmentToChangeDate={appointmentToChangeDate}
+                      appointmentsForDate={appointmentsForDate}
+                      setAppointmentToChange={setAppointmentToChange}
+                      setAppointmentToChangeDate={setAppointmentToChangeDate}
+                      handleEditAppointment={handleEditAppointment}
+                      appointmentNewPatient={appointmentNewPatient}
+                      setAppointmentNewPatient={setAppointmentNewPatient}
+                      setAppointmentNewDate={setAppointmentNewDate}
+                      handleSecretaryDeleteAppointment={handleSecretaryDeleteAppointment}
+                    />
+                  </div>
+                </div>
+    
+                <div className="flex w-full flex-1 flex-col items-center justify-center text-center">
+                  <div className="bg-white flex w-full">
+                      <DoctorsList doctors={doctors}></DoctorsList>
+                      <AppointmentsTable doctors={doctors} appointments={appointments} />
+                  </div>
+                </div>
+                </>
+              )
+                :
+              (
+                <div className="bg-white flex w-full mb-auto">
+                  <FormDoctorCreateAppointment 
+                    handleCreateAppointment={handleCreateAppointment}
+                    setAppointmentPatient={setAppointmentPatient}
+                    appointmentPatient={appointmentPatient}
+                    setAppointmentDate={setAppointmentDate}
+                    appointmentDate={appointmentDate}
+                  />
+                  <FormDoctorEditAppointment 
+                    appointments={appointments}
+                    appointmentToChange={appointmentToChange}
+                    appointmentToChangeDate={appointmentToChangeDate}
+                    appointmentsForDate={appointmentsForDate}
+                    setAppointmentToChange={setAppointmentToChange}
+                    setAppointmentToChangeDate={setAppointmentToChangeDate}
+                    handleEditAppointment={handleEditAppointment}
+                    appointmentNewPatient={appointmentNewPatient}
+                    setAppointmentNewPatient={setAppointmentNewPatient}
+                    setAppointmentNewDate={setAppointmentNewDate}
+                    handleSecretaryDeleteAppointment={handleSecretaryDeleteAppointment}
+                  />
+                </div>
+              )
+            }
           </div>
         </div>
     )
