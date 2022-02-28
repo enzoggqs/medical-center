80910b38ab6822808abd1bb2103e9c987d19af7d
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-02-28 01:09:42 -0300

    Dividing homePage in components

--- medical-center-front/src/pages/HomePage/index.jsx
+++ medical-center-front/src/pages/HomePage/index.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useEffect } from "react";
 import { useState } from "react";
 import AppointmentsTable from "../../components/AppointmentsTable";
 import DoctorsList from "../../components/DoctorsList";
+import FormAddDoctor from "../../components/FormAddDoctor";
+import FormEditDoctor from "../../components/FormEditDoctor";
+import FormSecretaryCreateAppointment from "../../components/FormSecretaryCreateAppointment";
 import Header from "../../components/Header";
 
 import { AuthContext } from "../../contexts/auth";
@@ -42,13 +45,14 @@ const HomePage = () => {
     const [appointmentNewDate, setAppointmentNewDate] = useState('');
 
     useEffect(() => {
-        console.log(user);
+        console.log(user, appointments);
         if(user.userType === 2){
             (async () => {
                 const allDoctors = await getDoctors();
                 setDoctors(allDoctors.data.doctors);
 
                 const allAppointments =  await getSecretaryAppointments();
+                console.log(allAppointments.data, allDoctors.data)
                 setAppointments(allAppointments.data);
                 
                 setLoading(false);
@@ -87,6 +91,7 @@ const HomePage = () => {
     }, [appointmentToChange])
 
     const handleCreateDoctor = async (e) => {
+        console.log('oi')
         e.preventDefault();
 
         await createDoctor(doctorToCreateName, doctorToCreateEmail)
@@ -185,164 +190,38 @@ const HomePage = () => {
           <div className="flex w-full flex-1 flex-col items-center justify-center text-center">
             <Header />
             <div className="bg-white flex w-full">
-              <form onSubmit={handleCreateDoctor} className="w-1/2 p-5">
-                <h2 className="font-bold">Menu Doutor</h2>
-                <div className="py-10">
-                  <div className="py-10">
-                    <h2 className="text-3xl font-bold text-red-400 mb-2">Cadastrar Doutor</h2>
-                    <div className="border-2 w-20 border-red-400 inline-block"></div>
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <div className="bg-gray-100 mx-auto 2xl:w-1/2 md:w-64 p-2 flex items-center mb-3">
-                      <input 
-                        autoComplete="off"
-                        name="doctorName" 
-                        placeholder="Name" 
-                        className="bg-gray-100 outline-none flex-1"
-                        onChange={e => setDoctorToCreateName(e.target.value)}
-                        value={doctorToCreateName}
-                        required
-                      ></input>
-                    </div>
-                    <div className="bg-gray-100 mx-auto 2xl:w-1/2 md:w-64 p-2 flex items-center mb-6">
-                      <input
-                        type="email" 
-                        name="doctorEmail" 
-                        placeholder="Email" 
-                        className="bg-gray-100 outline-none flex-1"
-                        value={doctorToCreateEmail}
-                        onChange={e => setDoctorToCreateEmail(e.target.value)}
-                        required
-                      ></input>
-                    </div>
-                    <button 
-                    type="submit"
-                    className="border-2 border-red-400 text-red-400 rounded-full px-12 py-2 inline-block font-semibold hover:bg-red-400 hover:text-white"
-                    >
-                      Cadastrar
-                    </button>
-                  </div>
-                </div>
-              </form> 
-
-              <div className="w-1/2 bg-red-400 text-white py-5">
-                <h2 className="text-black font-bold">Menu Consultas</h2>
-                <form onSubmit={handleCreateAppointment}>
-                  <h2 className="text-3xl font-bold mb-5">Criar consulta</h2>
-                  <div className="border-2 w-20 border-white inline-block mb-10"></div>
-                  <div className="mx-auto 2xl:w-1/2 md:w-64 bg-gray-100 p-2 flex items-center mb-5">
-                    <input 
-                      name="patientName" 
-                      placeholder="Nome do paciente" 
-                      className="bg-gray-100 outline-none flex-1 text-black"
-                      onChange={e => setAppointmentPatient(e.target.value)}
-                      value={appointmentPatient}
-                      required             
-                    ></input>
-                  </div>
-                  <div className="mx-auto 2xl:w-1/2 md:w-64 bg-gray-100 p-2 flex items-center mb-5">
-                    <select 
-                      name="doctorName" 
-                      placeholder="Nome do Doutor" 
-                      className="bg-gray-100 outline-none flex-1 text-black"
-                      onChange={e => setAppointmentDoctor(e.target.value)}
-                      value={appointmentDoctor ? appointmentDoctor : "DEFAULT"}
-                      required        
-                    >
-                      <option value="DEFAULT" disabled>Nome do Doutor</option>
-                      {
-                          doctors.map((doctor) => (
-                            <option key={doctor.id} value={doctor.id}>{doctor.name}</option>
-                          ))
-                      }
-                    </select>
-                  </div>
-                  <div className="mx-auto 2xl:w-1/2 md:w-64 bg-gray-100 p-2 flex items-center mb-5">
-                    <input 
-                      type="datetime-local"
-                      name="appointmentDate" 
-                      placeholder="Data da Consulta" 
-                      className="bg-gray-100 outline-none flex-1 text-black"
-                      onChange={e => setAppointmentDate(e.target.value)}
-                      value={appointmentDate}
-                      required             
-                    ></input>
-                  </div>
-                  <button 
-                    type="submit"
-                    className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-red-400"
-                  >
-                    Criar
-                  </button>
-                </form>
-              </div>
+              <FormAddDoctor 
+                setDoctorToCreateName={setDoctorToCreateName} 
+                setDoctorToCreateEmail={setDoctorToCreateEmail} 
+                doctorToCreateName={doctorToCreateName} 
+                doctorToCreateEmail={doctorToCreateEmail}
+                handleCreateDoctor={handleCreateDoctor}
+              />
+              <FormSecretaryCreateAppointment
+                setAppointmentDoctor={setAppointmentDoctor}
+                setAppointmentPatient={setAppointmentPatient}
+                setAppointmentDate={setAppointmentDate}
+                doctors={doctors}
+                appointmentDoctor={appointmentDoctor}
+                appointmentPatient={appointmentPatient}
+                appointmentDate={appointmentDate}
+                handleCreateAppointment={handleCreateAppointment}
+              />     
             </div>
 
             <div className="flex w-full flex-1 flex-col items-center justify-center text-center">
               <div className="bg-white flex w-full">
-                <div className="w-1/2 py-5 px-28">
-                  <div className="py-5">
-                    <div className="py-5">
-                      <h2 className="text-3xl font-bold text-red-400 mb-2">Editar/Excluir Doutor</h2>
-                      <div className="border-2 w-20 border-red-400 inline-block"></div>
-                    </div>
-                  </div>
-                  <h2 className="text-lg font-bold text-red-400 mb-4">Selecione o doutor que deseja modificar ou excluir</h2>
-                  <div className="2xl:w-1/2 md:w-64 mx-auto bg-gray-100 p-2 flex items-center mb-8">
-                    <select
-                      name="doctorName" 
-                      placeholder="Nome do Doutor" 
-                      className="bg-gray-100 outline-none flex-1 text-black"
-                      onChange={e => setDoctorToChange(e.target.value)}
-                      required 
-                      value={doctorToChange ? doctorToChange : "DEFAULT"}       
-                      >
-                      <option value="DEFAULT" disabled>Selecione o nome do doutor</option>
-                      {
-                          doctors.map((doctor) => (
-                            <option key={doctor.id} value={doctor.id}>{doctor.name}</option>
-                          ))
-                      }
-                    </select>
-                  </div>
-                  
-                  {
-                    showDoctorToChangeFields && (
-                      <form className="2xl:w-1/2 md:w-64 mx-auto" onSubmit={handleEditDoctor}>
-                        <h2 className="text-lg font-bold text-red-400 mb-4">Novo nome do doutor</h2>
-                        <div className="bg-gray-100 p-2 flex items-center mb-5">
-                          <input 
-                            name="patientName" 
-                            placeholder="Novo nome do doutor" 
-                            className="bg-gray-100 outline-none flex-1 text-black"
-                            value={doctorNewName}
-                            onChange={e => setDoctorNewName(e.target.value)} 
-                            required         
-                          ></input>
-                        </div>
-                        <div className="flex-col">
-                          <button 
-                          type="submit"
-                          className="mb-5 mt-5 border-2 border-red-400 text-red-400 rounded-full px-12 py-2 inline-block font-semibold hover:bg-red-400 hover:text-white"
-                          >
-                            Editar
-                          </button> 
-                        </div>
-                        <div className="flex-col">
-                          <button 
-                          onClick={handleDeleteDoctor}
-                          className="border-2 border-red-400 text-red-400 rounded-full px-12 py-2 inline-block font-semibold hover:bg-red-400 hover:text-white"
-                          >
-                            Excluir
-                          </button>
-                        </div>
-                      </form> 
-                    )
-                  }
-
-
-                  </div>
-                  <div className="w-1/2 bg-red-400 text-white py-5 px-28">
+                <FormEditDoctor 
+                  setDoctorToChange={setDoctorToChange}
+                  doctorToChange={doctorToChange}
+                  doctors={doctors}
+                  showDoctorToChangeFields={showDoctorToChangeFields}
+                  handleEditDoctor={handleEditDoctor}
+                  doctorNewName={doctorNewName}
+                  setDoctorNewName={setDoctorNewName}
+                  handleDeleteDoctor={handleDeleteDoctor}
+                />
+                <div className="w-1/2 bg-red-400 text-white py-5 px-28">
                   <div className="py-5">
                     <div className="py-5">
                       <h2 className="text-3xl font-bold text-white mb-2">Editar/Excluir Consulta</h2>
@@ -351,11 +230,11 @@ const HomePage = () => {
                   </div>
                   <h2 className="text-lg font-bold text-white mb-4">Selecione a consulta que deseja modificar ou excluir</h2>
                   <h2 className="text-lg font-bold text-white mb-4 pt-4">Data</h2>
-                  <div className="bg-gray-100 p-2 flex items-center mb-4 mx-auto 2xl:w-1/2 md:w-64 mx-auto">
+                  <div className="bg-gray-100 p-2 rounded-full border-2 flex items-center mb-4 mx-auto 2xl:w-1/2 md:w-64 mx-auto">
                     <select
                       name="appointmentDate" 
                       placeholder="Nome do Doutor" 
-                      className="bg-gray-100 outline-none flex-1 text-black"
+                      className="pl-2 bg-gray-100 outline-none flex-1 text-black"
                       onChange={e => setAppointmentToChangeDate(e.target.value)}
                       required
                       value={appointmentToChangeDate ? appointmentToChangeDate : "DEFAULT"}
@@ -372,11 +251,11 @@ const HomePage = () => {
                     appointmentsForDate && (
                       <div>
                       <h2 className="text-lg font-bold text-white mb-4">Paciente</h2>
-                        <div className="bg-gray-100 p-2 flex items-center mb-8 2xl:w-1/2 md:w-64 mx-auto">
+                        <div className="rounded-full border-2 bg-gray-100 p-2 flex items-center mb-8 2xl:w-1/2 md:w-64 mx-auto">
                           <select
                             name="doctorName" 
                             placeholder="Nome do Doutor" 
-                            className="bg-gray-100 outline-none flex-1 text-black"
+                            className="pl-2 bg-gray-100 outline-none flex-1 text-black"
                             onChange={e => setAppointmentToChange(e.target.value)}
                             required
                             value={appointmentToChange ? appointmentToChange : "DEFAULT"}            
@@ -396,22 +275,22 @@ const HomePage = () => {
                     appointmentToChange && (
                       <form className="mx-auto 2xl:w-1/2 md:w-64" onSubmit={handleEditAppointment}>
                         <h2 className="text-lg font-bold text-white mb-4">Novo nome do paciente</h2>
-                        <div className="bg-gray-100 p-2 flex items-center mb-5">
+                        <div className="border-2 rounded-full bg-gray-100 p-2 flex items-center mb-5">
                           <input 
                             name="newPatientName" 
                             placeholder="Novo nome do paciente" 
-                            className="bg-gray-100 outline-none flex-1 text-black"
+                            className="pl-2 bg-gray-100 outline-none flex-1 text-black"
                             value={appointmentNewPatient}
                             onChange={e => setAppointmentNewPatient(e.target.value)} 
                             required         
                           ></input>
                         </div>
                         <h2 className="text-lg font-bold text-white mb-4">Nova data da consulta</h2>
-                        <div className="bg-gray-100 p-2 flex items-center mb-5">
+                        <div className="rounded-full border-2 bg-gray-100 p-2 flex items-center mb-5">
                           <input
                             type="date"
                             name="newAppointmentDate" 
-                            className="bg-gray-100 outline-none flex-1 text-black"
+                            className="pl-2 bg-gray-100 outline-none flex-1 text-black"
                             // value={appointmentNewDate}
                             onChange={e => setAppointmentNewDate(e.target.value)}         
                           ></input>
@@ -419,13 +298,13 @@ const HomePage = () => {
                         <div className="flex-row">
                           <button 
                           type="submit"
-                          className="mb-5 mt-5 border-2 border-white text-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-red-400 hover:text-white"
+                          className="hover:bg-white hover:text-red-400 mb-5 mt-5 border-2 border-white text-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-red-400 hover:text-white"
                           >
                             Editar
                           </button>
                           <button 
                           onClick={handleSecretaryDeleteAppointment}
-                          className="border-2 border-white text-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-red-400 hover:text-white"
+                          className="hover:bg-white hover:text-red-400 border-2 border-white text-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-red-400 hover:text-white"
                           >
                             Excluir
                           </button>
@@ -447,16 +326,6 @@ const HomePage = () => {
                     </div>
                   </div>
                   <DoctorsList doctors={doctors}></DoctorsList>
-                  {/* <ul>
-                    {doctors.map((doctor) => (
-                        <li className="text-xl py-1 text-red-400"
-                            key={doctor.id}>
-                            {doctor.id} -
-                            {doctor.email} -
-                            {doctor.name}
-                        </li>
-                    ))}
-                  </ul> */}
                 </div>
                 <div className="w-1/2 bg-red-400 text-white py-8">
                   <div className="py-10">
